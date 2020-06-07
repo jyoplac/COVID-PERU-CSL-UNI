4389dc7f52c1715878e456547bfd8c5cc53f6d8c
jyoplac <jhon_yoplac@outlook.com>
2020-06-07 16:33:06 -0500

    alfa 2.992
    data 7-jun

--- SIR Basico.py
+++ SIR Basico.py
@@ -239,9 +239,9 @@ fs.json_js(arch_json='tabla2.json', arch_js='tabla2.js', nombre='tabla2')
 # tabla 3
 aux_dia = str(1 + cuarentena[-1])
 aux_2 = 'Día ' + aux_dia + ' de cuarentena'
-aux_fec = '07-jun'
-aux_pro_con = '193892'+' ± '+'0.600'+'%'
-aux_pro_recup = '85799'+' ± '+'1.685'+'%'
+aux_fec = '08-jun'
+aux_pro_con = '198648'+' ± '+'0.604'+'%'
+aux_pro_recup = '89743'+' ± '+'1.605'+'%'
 tabla3 = [
     ['Pronóstico', 'Fecha', 'Contagiados', 'Recuperados'],
     [aux_2, aux_fec, aux_pro_con, aux_pro_recup]
